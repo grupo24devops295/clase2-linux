731c6cd9bdc79550dd56e4ae379eebfbf84d0edf
vramirez0113 <vlakstarit@gmail.com>
2023-11-19 20:34:50 -0500

    added discordd channel notification

--- deploy-grupo2.sh
+++ deploy-grupo2.sh
@@ -285,21 +285,18 @@ curl -X POST -H "Content-Type: application/json" -d "{\"content\":\"$message\"}"
 
 # Send Notification to DevOps295 Discor Channel
 # Configura el token de acceso de tu bot de Discord
-#DISCORD=""
-
+#Configura el token de acceso de tu bot de Discord
+DISCORD="https://discord.com/api/webhooks/1169002249939329156/7MOorDwzym-yBUs3gp0k5q7HyA42M5eYjfjpZgEwmAx1vVVcLgnlSh4TmtqZqCtbupov"
 # Verifica si se proporcionó el argumento del directorio del repositorio
-if [ $# -ne 1 ]; then
-  echo "Uso: $0 <ruta_al_repositorio>"
-  exit 1
+cd $SCRIPT_DIR
+git config --global --add safe.directory "$SCRIPT_DIR/$REPO_NAME"
+if [ -d $REPO_DIR ]; then
+   cd $REPO_NAME
 fi
-
-# Cambia al directorio del repositorio
-cd "$1"
-
 # Obtiene el nombre del repositorio
-REPO_NAME=$(basename $(git rev-parse --show-toplevel))
+repo_name=$(basename $(git rev-parse --show-toplevel))
 # Obtiene la URL remota del repositorio
-REPO_URL=$(git remote get-url origin)
+repo_url=$(git remote get-url origin)
 WEB_URL="localhost"
 # Realiza una solicitud HTTP GET a la URL
 HTTP_STATUS=$(curl -Is "$WEB_URL" | head -n 1)
@@ -307,7 +304,7 @@ HTTP_STATUS=$(curl -Is "$WEB_URL" | head -n 1)
 # Verifica si la respuesta es 200 OK (puedes ajustar esto según tus necesidades)
 if [[ "$HTTP_STATUS" == *"200 OK"* ]]; then
   # Obtén información del repositorio
-    DEPLOYMENT_INFO2="Despliegue del repositorio $REPO_NAME: "
+    DEPLOYMENT_INFO2="Despliegue del repositorio $repo_name: "
     DEPLOYMENT_INFO="La página web $WEB_URL está en línea."
     COMMIT="Commit: $(git rev-parse --short HEAD)"
     AUTHOR="Autor: $(git log -1 --pretty=format:'%an')"
@@ -316,14 +313,8 @@ else
   DEPLOYMENT_INFO="La página web $WEB_URL no está en línea."
 fi
 
-# Obtén información del repositorio
-
-
 # Construye el mensaje
 MESSAGE="$DEPLOYMENT_INFO2\n$DEPLOYMENT_INFO\n$COMMIT\n$AUTHOR\n$REPO_URL\n$DESCRIPTION"
 
 # Envía el mensaje a Discord utilizando la API de Discord
-curl -X POST -H "Content-Type: application/json" \
-     -d '{
-       "content": "'"${MESSAGE}"'"
-     }' "$DISCORD"
+curl -X POST -H "Content-Type: application/json" -d "{\"content\":\"$MESSAGE\"}" "$DISCORD"
